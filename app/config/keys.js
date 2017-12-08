084e41236548ad071ae3ae2aa6dc0993b9fd08db
Scott Montague <SMontague29@gmail.com>
2017-12-08 16:23:41 -0700

    Delete keys.js

--- app/config/keys.js
+++
@@ -1,7 +0,0 @@
-var keys = {
-    "mapKey":"AIzaSyA03U9qq_vhGXvij2jxJjE3gE1v1KaZlRg",
-    "s3bucket": "willow-project",
-    "s3accesskey": "AKIAJNUHLPY4BVNOMDBQ",
-    "s3secretaccesskey": "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",
-};
-module.exports = keys;
